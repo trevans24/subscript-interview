8b612d50104be09724ad2dd501e1b2b23bd9b3af
trevans24 <trevans24@gmail.com>
2025-02-07 09:26:18 -0700

    cleaned up header component, alphabetized styliing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,18 @@
 import React from "react"
 
-const Header = () => {
-  const headerStyle = {
-    padding: "20px 0",
-    lineHeight: "2em",
-  }
-  return (
-    <header style={headerStyle}>
-      <h1 className="header" style={{ fontSize: "25px", marginBottom: "15px" }}>
-        Simple Todo App
-      </h1>
-      <p style={{ fontSize: "19px" }}>
-        Please add to-dos item(s) through the input field
-      </p>
-    </header>
-  )
+const headerStyle = {
+  lineHeight: "2em",
+  padding: "20px 0",
 }
 
+const Header = () => (
+  <header style={headerStyle}>
+    <h1 className="header" style={{ fontSize: "25px", marginBottom: "15px" }}>
+      Simple Todo App
+    </h1>
+    <p style={{ fontSize: "19px" }}>
+      Please add to-dos item(s) through the input field
+    </p>
+  </header>
+)
 export default Header
