048f51965f7457bd3f2a0de79331fe5272c835e3
trevans24 <trevans24@gmail.com>
2025-02-07 09:59:02 -0700

    todo item memoized, useCallback for function calls

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useCallback } from "react"
 
 const completedStyle = {
   color: "#d35e0f",
@@ -12,17 +12,19 @@ const containerStyle = {
   gap: "4px",
 }
 
-const TodoItem = ({ deleteTodoProps, handleChangeProps, todo }) => {
+const TodoItem = React.memo(({ deleteTodoProps, handleChangeProps, todo }) => {
   const { completed, id, title, tags } = todo
+
+  const handleChange = useCallback(
+    () => handleChangeProps(id),
+    [handleChangeProps]
+  )
+  const handleDelete = useCallback(() => deleteTodoProps(id), [deleteTodoProps])
 
   return (
     <li className="todo-item">
-      <input
-        type="checkbox"
-        checked={completed}
-        onChange={() => handleChangeProps(id)}
-      />
-      <button onClick={() => deleteTodoProps(id)}>Delete</button>
+      <input type="checkbox" checked={completed} onChange={handleChange} />
+      <button onClick={handleDelete}>Delete</button>
       <span style={completed ? completedStyle : null}>{title}</span>
       <div style={containerStyle}>
         {tags.map((tag) => (
@@ -31,6 +33,6 @@ const TodoItem = ({ deleteTodoProps, handleChangeProps, todo }) => {
       </div>
     </li>
   )
-}
+})
 
 export default TodoItem
