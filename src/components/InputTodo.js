bacc7d7b3d1961b0366d58c2a74893d99a74da62
trevans24 <trevans24@gmail.com>
2025-02-07 09:28:54 -0700

    alphabetized input

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -14,12 +14,12 @@ const InputTodo = ({ addTodoProps }) => {
   return (
     <form onSubmit={handleSubmit} className="form-container">
       <input
-        type="text"
         className="input-text"
-        placeholder="Add todo..."
-        value={title}
         name="title"
         onChange={onChange}
+        placeholder="Add todo..."
+        type="text"
+        value={title}
       />
       <input type="submit" className="input-submit" value="Submit" />
     </form>
