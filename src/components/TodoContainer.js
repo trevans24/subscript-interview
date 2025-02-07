4717827335e088db35471753cce3fe7b90a3c09b
trevans24 <trevans24@gmail.com>
2025-02-07 09:37:36 -0700

    todo container as a functional component, added useReducer, starting to update functions for todo

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,79 +1,88 @@
-import React from "react";
-import TodosList from "./TodosList";
-import Header from "./Header";
-import InputTodo from "./InputTodo";
+import React, { useReducer } from "react"
+import TodosList from "./TodosList"
+import Header from "./Header"
+import InputTodo from "./InputTodo"
 // import uuid from "uuid";
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from "uuid"
 
-class TodoContainer extends React.Component {
-  state = {
-    todos: [
-      {
-        // id: uuid.v4(),
-        id: uuidv4(),
-        title: "Setup development environment",
-        completed: true,
-      },
-      {
-        // id: uuid.v4(),
-        id: uuidv4(),
-        title: "Develop website and add content",
-        completed: false,
-      },
-      {
-        // id: uuid.v4(),
-        id: uuidv4(),
-        title: "Deploy to live server",
-        completed: false,
-      },
-    ],
-  };
+const baseTodos = [
+  {
+    // id: uuid.v4(),
+    id: uuidv4(),
+    title: "Setup development environment",
+    completed: true,
+  },
+  {
+    // id: uuid.v4(),
+    id: uuidv4(),
+    title: "Develop website and add content",
+    completed: false,
+  },
+  {
+    // id: uuid.v4(),
+    id: uuidv4(),
+    title: "Deploy to live server",
+    completed: false,
+  },
+]
 
-  handleChange = (id) => {
+const todoReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      return action.payload
+    case "DELETE_TODO":
+      return action.payload
+    case "TOGGLE_TODO":
+      return action.payload
+    default:
+      return state
+  }
+}
+
+const TodoContainer = () => {
+  const [todos, dispatch] = useReducer(todoReducer, baseTodos)
+
+  const handleChange = (id) => {
     this.setState({
       todos: this.state.todos.map((todo) => {
         if (todo.id === id) {
-          todo.completed = !todo.completed;
+          todo.completed = !todo.completed
         }
-        return todo;
+        return todo
       }),
-    });
-  };
+    })
+  }
 
-  delTodo = (id) => {
+  const delTodo = (id) => {
     this.setState({
       todos: [
         ...this.state.todos.filter((todo) => {
-          return todo.id !== id;
+          return todo.id !== id
         }),
       ],
-    });
-  };
+    })
+  }
 
-  addTodoItem = (title) => {
+  const addTodoItem = (title) => {
     const newTodo = {
       // id: uuid.v4(),
       id: uuidv4(),
       title: title,
       completed: false,
-    };
-    this.setState({
-      todos: [...this.state.todos, newTodo],
-    });
-  };
-
-  render() {
-    return (
-      <div className="container">
-        <Header />
-        <InputTodo addTodoProps={this.addTodoItem} />
-        <TodosList
-          todos={this.state.todos}
-          handleChangeProps={this.handleChange}
-          deleteTodoProps={this.delTodo}
-        />
-      </div>
-    );
+    }
+    dispatch({ type: "ADD_TODO", payload: [...todos, newTodo] })
   }
+
+  return (
+    <div className="container">
+      <Header />
+      <InputTodo addTodoProps={addTodoItem} />
+      <TodosList
+        deleteTodoProps={delTodo}
+        handleChangeProps={handleChange}
+        todos={todos}
+      />
+    </div>
+  )
 }
-export default TodoContainer;
+export default TodoContainer
